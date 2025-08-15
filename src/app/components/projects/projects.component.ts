b4d07d6c37792e5cd6686ee6572e328cf3ea3390
Kanishk <kanishkmangal2@gmail.com>
2025-08-15 14:24:37 +0530

    third commit

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -10,11 +10,33 @@ import { Product } from '../../model/product';
   styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
-  projects: Product[] = [];
+  projects = [
+    {
+      name: 'Lpu Survey Form',
+      detail: 'A web survey form for facilities.',
+      image: 'assets/images/Surveyform.png',
+      tech: ['Angular', 'Next.js', 'Node.js'],
+      url: 'https://github.com/yourusername/library-seat-booking'
+    },
+    {
+      name: 'Disk Scheduling Visualizer',
+      detail: 'A tool to visualize and compare disk scheduling algorithms.',
+      image: 'assets/images/disk.png',
+      tech: ['Angular', 'JavaScript'],
+      url: 'https://github.com/yourusername/disk-scheduling'
+    },
+    {
+      name: 'Portfolio Website',
+      detail: 'My personal portfolio to showcase projects and skills.',
+      image: 'assets/images/portfolio.png',
+      tech: ['Angular', 'Tailwind CSS'],
+      url: 'https://yourusername.github.io/portfolio'
+    }
+  ];
   httpService = inject(HttpService);
   ngOnInit() {
     this.httpService.getProjects().subscribe((result) => {
-      this.projects = result;
+      // this.projects = result;
       console.log(this.projects);
     });
   }
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+  import { Component, inject } from '@angular/core';
 import { HttpService } from '../../http.service';
 
 @Component({
@@ -8,7 +8,14 @@ import { HttpService } from '../../http.service';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
- skills: any[]=[];
+ skills=[
+    { name: 'JavaScript', exp: 2 },
+    { name: 'Python', exp: 1.5 },
+    { name: 'C++', exp: 1.5 },
+    { name: 'Java', exp: 2 },
+    { name: 'HTML/CSS', exp: 2 },
+    { name: 'SQL', exp: 1 }
+ ];
  httpService = inject(HttpService);
  ngOnInit(){
    this.httpService.getSkills().subscribe(result=>{
